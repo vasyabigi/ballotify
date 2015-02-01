5ab16c0d1c17df7bcdcff5a2d41a953b1a2a7bb4
Vasyl Stanislavchuk <vasyl.stanislavchuk@djangostars.com>
2015-02-01 22:10:24 +0200

    Removed api prefix from the api.

--- ballotify/urls.py
+++ ballotify/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns(
     url(r'^', include('rest_framework_swagger.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/v1/', include('api_v1.urls')),
+    url(r'^v1/', include('api_v1.urls')),
 )
